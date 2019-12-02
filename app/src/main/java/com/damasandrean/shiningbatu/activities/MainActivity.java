fb6cf439d3261f9fb43f19c8972d7bbde6dfe5e4
Damas Andrean <andreandamas@gmail.com>
2019-12-02 20:06:19 +0700

    Menambahkan Splash Screen

--- app/src/main/java/com/damasandrean/shiningbatu/activities/MainActivity.java
+++ app/src/main/java/com/damasandrean/shiningbatu/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.damasandrean.shiningbatu;
+package com.damasandrean.shiningbatu.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.damasandrean.shiningbatu.R;
 
 public class MainActivity extends AppCompatActivity {
 
